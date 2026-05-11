e003dbc8a97b19a1d899779bfbed03d7cb3de37e
S. Inga Helgadottir <cph-sh557@cphbusiness.dk>
2026-05-11 10:27:08 +0200

    start code

---
+++ src/main/java/codecvbackend/CodeCvBackEndApplication.java
@@ -0,0 +1,13 @@
+package codecvbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeCvBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeCvBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/codecvbackend/CodeCvBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package codecvbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeCvBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
