39ec4968df43500912a47a6576d783d9ac0994d1
S. Inga Helgadottir <cph-sh557@cphbusiness.dk>
2026-05-11 10:54:32 +0200

    created emtities

--- src/main/java/codecvbackend/Entity/Shift.java
+++ src/main/java/codecvbackend/Entity/Shift.java
@@ -1,4 +1,29 @@
 package codecvbackend.Entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "shift")
 public class Shift {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "department")
+    private String department;
+    @Column(name = "startTime")
+    private LocalDateTime startTime;
+    @Column(name = "endTime")
+    private LocalDateTime endTime;
+    @Column(name = "ammountOfEmployeesNeeded")
+    private LocalDateTime ammountOfEmployeesNeeded;
 }
