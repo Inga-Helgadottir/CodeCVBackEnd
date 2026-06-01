8e30a3338748f8c0e43693948d113db4a4908d2e
S. Inga Helgadottir <cph-sh557@cphbusiness.dk>
2026-06-01 12:31:33 +0200

    Entities

--- src/main/java/codecvbackend/Entity/Employee.java
+++ src/main/java/codecvbackend/Entity/Employee.java
@@ -1,16 +1,17 @@
 package codecvbackend.Entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@ToString
 @Table(name = "employees")
 public class Employee {
     @Id
@@ -20,7 +21,26 @@ public class Employee {
     private String firstName;
     @Column(name = "last_name")
     private String lastName;
-    @Column(name = "email_id", nullable = false, unique = true)
+    @Column(name = "email", nullable = false)
     private String email;
+    @ManyToMany(mappedBy = "employeeShifts")
+    private Set<Shift> shifts = new HashSet<>();
 
+    public Employee(String firstName, String lastName, String email, Set<Shift> shifts) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.shifts = shifts;
+    }
+
+    public Employee(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public void addShiftToEmployee(Shift shift) {
+        this.shifts.add(shift);
+        shift.addEmployeeToShift(this);
+    }
 }
--- src/main/java/codecvbackend/Entity/Shift.java
+++ src/main/java/codecvbackend/Entity/Shift.java
@@ -1,18 +1,18 @@
 package codecvbackend.Entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@ToString(exclude = {"employeesOnShifts"})
 @Table(name = "shift")
 public class Shift {
     @Id
@@ -25,5 +25,51 @@ public class Shift {
     @Column(name = "endTime")
     private LocalDateTime endTime;
     @Column(name = "ammountOfEmployeesNeeded")
-    private LocalDateTime amountOfEmployeesNeeded;
+    private Long amountOfEmployeesNeeded;
+    @ManyToMany
+    @JoinTable(name = "employeeShifts",
+            joinColumns = @JoinColumn(name = "shiftId"),
+            inverseJoinColumns = @JoinColumn(name = "employeeId")
+    )
+    private Set<Employee> employeesOnShifts = new HashSet<>();
+
+    public Shift(String department, LocalDateTime startTime, LocalDateTime endTime, Boolean areThereEnoughEmployees, Long amountOfEmployeesNeeded) {
+        this.department = department;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.amountOfEmployeesNeeded = amountOfEmployeesNeeded;
+    }
+
+    public Shift(String department, LocalDateTime startTime, LocalDateTime endTime, Long amountOfEmployeesNeeded, Set<Employee> employeesOnShifts) {
+        this.department = department;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.amountOfEmployeesNeeded = amountOfEmployeesNeeded;
+        this.employeesOnShifts = employeesOnShifts;
+    }
+
+    public Set<Long> getEmployeeIds() {
+        Set<Long> employeeIds = new HashSet<>();
+        Set<Employee> employees = this.employeesOnShifts;
+
+        for (Employee employee : employees) {
+            employeeIds.add(employee.getId());
+        }
+        return employeeIds;
+    }
+
+
+    public void addEmployeeToShift(Employee employee){
+        this.employeesOnShifts.add(employee);
+        areThereEnoughEmployees();
+    }
+
+
+    public boolean areThereEnoughEmployees(){
+        if(this.amountOfEmployeesNeeded == employeesOnShifts.size()){
+            return true;
+        }else{
+            return false;
+        }
+    }
 }
