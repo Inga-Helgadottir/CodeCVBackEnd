a2287154a7d63e19acc186eaf2d3a0f92839a585
S. Inga Helgadottir <cph-sh557@cphbusiness.dk>
2026-06-08 13:26:01 +0200

    cleanup

--- src/main/java/codecvbackend/CodeCvBackEndApplication.java
+++ src/main/java/codecvbackend/CodeCvBackEndApplication.java
@@ -1,5 +1,4 @@
 package codecvbackend;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,5 +8,5 @@ public class CodeCvBackEndApplication {
     public static void main(String[] args) {
         SpringApplication.run(CodeCvBackEndApplication.class, args);
     }
-
 }
+
